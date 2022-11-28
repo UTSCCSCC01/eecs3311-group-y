4d5b26d0f2ba15bc688624caf4ea995ce1e1b397
samoon-abdul <77294717+samoon-abdul@users.noreply.github.com>
2022-11-27 20:03:11 -0500

    Added visualizations

---
+++ src/main/java/visualizations/Viewer_Scatter.java
@@ -0,0 +1,253 @@
+package visualizations;
+
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.time.Year;
+import dataFetch.DataAcquisition;
+import dataFetch.StoredData;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Font;
+import java.util.ArrayList;
+
+public class Viewer_Scatter extends JFrame {
+
+    private static final long serialVersionUID = 6043180864004509049L;
+    private ArrayList<StoredData> dataStorage;
+    private StoredData data, data2, data3;
+    private String title;
+    private String xLabel, yLabel, yLabel2;
+    public String series1;
+    private String series2;
+    private String series3;
+    private JFreeChart scatterChart;
+    private ChartPanel chartPanel;
+    private int vers;
+    private XYPlot plot;
+
+    public Viewer_Scatter(ArrayList<StoredData> dataStorage, String xLabel, String yLabel, String title) {
+        if (dataStorage.equals(null)) {
+            return;
+        }
+        this.xLabel = xLabel;
+        this.yLabel = yLabel;
+        this.yLabel2 = yLabel;
+        this.title = title;
+        this.dataStorage = dataStorage;
+        draw();
+
+    }
+
+    public Viewer_Scatter(ArrayList<StoredData> dataStorage, String xLabel, String yLabel, String yLabel2,
+            String title) {
+        if (dataStorage.equals(null)) {
+            return;
+        }
+        this.xLabel = xLabel;
+        this.yLabel = yLabel;
+        this.yLabel2 = yLabel2;
+        this.title = title;
+        this.dataStorage = dataStorage;
+        draw();
+
+    }
+
+    private void draw() {
+        switch (dataStorage.size()) {
+            case 0:
+                break;
+            case 1:
+                this.data = dataStorage.get(0);
+                this.series1 = data.getSeriesName();
+                this.vers = 1;
+                break;
+            case 2:
+                this.data = dataStorage.get(0);
+                this.data2 = dataStorage.get(1);
+                this.series1 = data.getSeriesName();
+                this.series2 = data2.getSeriesName();
+                this.vers = 2;
+                break;
+            case 3:
+                this.data = dataStorage.get(0);
+                this.data2 = dataStorage.get(1);
+                this.data3 = dataStorage.get(2);
+                this.series1 = data.getSeriesName();
+                this.series2 = data2.getSeriesName();
+                this.series3 = data3.getSeriesName();
+                this.vers = 3;
+                break;
+
+        }
+        pop();
+    }
+
+    public void pop() {
+        TimeSeries scatterSeries1, scatterSeries2, scatterSeries3;
+        TimeSeriesCollection dataset, dataset2;
+        XYItemRenderer itemrenderer1, itemrenderer2;
+        DateAxis domainAxis;
+
+        switch (vers) {
+            case 0:
+                break;
+            case 1:
+                scatterSeries1 = new TimeSeries(series1);
+                for (int i = 0; i < data.getValues().size(); i++) {
+                    scatterSeries1.add(new Year(data.getYears().get(i)), data.getValues().get(i));
+                }
+                dataset = new TimeSeriesCollection();
+                dataset.addSeries(scatterSeries1);
+                plot = new XYPlot();
+                itemrenderer1 = new XYLineAndShapeRenderer(false, true);
+                plot.setDataset(0, dataset);
+                plot.setRenderer(0, itemrenderer1);
+                domainAxis = new DateAxis(xLabel);
+                plot.setDomainAxis(domainAxis);
+                plot.setRangeAxis(new NumberAxis(yLabel));
+
+                plot.mapDatasetToRangeAxis(0, 0);// 1st dataset to 1st y-axis
+                plot.mapDatasetToRangeAxis(1, 1); // 2nd dataset to 2nd y-axis
+
+                scatterChart = new JFreeChart(title,
+                        new Font("Serif", java.awt.Font.BOLD, 18), plot, true);
+
+                createChart();
+                // to see uncomment this and the code in main
+                 seeExample();
+
+                break;
+            case 2:
+
+                scatterSeries1 = new TimeSeries(series1);
+                scatterSeries2 = new TimeSeries(series2);
+
+                for (int i = 0; i < data.getValues().size(); i++) {
+                    scatterSeries1.add(new Year(data.getYears().get(i)), data.getValues().get(i));
+                }
+                for (int i = 0; i < data2.getValues().size(); i++) {
+                    scatterSeries2.add(new Year(data2.getYears().get(i)), data2.getValues().get(i));
+                }
+                dataset = new TimeSeriesCollection();
+                dataset.addSeries(scatterSeries1);
+
+                dataset2 = new TimeSeriesCollection();
+                dataset2.addSeries(scatterSeries2);
+
+                plot = new XYPlot();
+                itemrenderer1 = new XYLineAndShapeRenderer(false, true);
+                itemrenderer2 = new XYLineAndShapeRenderer(false, true);
+
+                plot.setDataset(0, dataset);
+                plot.setRenderer(0, itemrenderer1);
+                domainAxis = new DateAxis(this.xLabel);
+                plot.setDomainAxis(domainAxis);
+                plot.setRangeAxis(new NumberAxis(this.yLabel));
+
+                plot.setDataset(1, dataset2);
+                plot.setRenderer(1, itemrenderer2);
+                plot.setRangeAxis(1, new NumberAxis(this.yLabel2));
+
+                plot.mapDatasetToRangeAxis(0, 0);// 1st dataset to 1st y-axis
+                plot.mapDatasetToRangeAxis(1, 1); // 2nd dataset to 2nd y-axis
+
+                scatterChart = new JFreeChart(title,
+                        new Font("Serif", java.awt.Font.BOLD, 18), plot, true);
+                createChart();
+                // to see uncomment this and the code in main
+                 seeExample();
+
+                break;
+            case 3:
+                scatterSeries1 = new TimeSeries(series1);
+                scatterSeries2 = new TimeSeries(series2);
+                scatterSeries3 = new TimeSeries(series3);
+
+                for (int i = 0; i < data.getValues().size(); i++) {
+                    scatterSeries1.add(new Year(data.getYears().get(i)), data.getValues().get(i));
+                }
+                for (int i = 0; i < data2.getValues().size(); i++) {
+                    scatterSeries2.add(new Year(data2.getYears().get(i)), data2.getValues().get(i));
+                }
+                for (int i = 0; i < data3.getValues().size(); i++) {
+                    scatterSeries3.add(new Year(data3.getYears().get(i)), data3.getValues().get(i));
+                }
+                dataset = new TimeSeriesCollection();
+                dataset.addSeries(scatterSeries1);
+
+                dataset2 = new TimeSeriesCollection();
+                dataset2.addSeries(scatterSeries2);
+
+                plot = new XYPlot();
+                itemrenderer1 = new XYLineAndShapeRenderer(false, true);
+                itemrenderer2 = new XYLineAndShapeRenderer(false, true);
+
+                dataset.addSeries(scatterSeries3);
+
+                plot.setDataset(0, dataset);
+                plot.setRenderer(0, itemrenderer1);
+                domainAxis = new DateAxis(this.xLabel);
+                plot.setDomainAxis(domainAxis);
+                plot.setRangeAxis(new NumberAxis(this.yLabel));
+
+                plot.setDataset(1, dataset2);
+                plot.setRenderer(1, itemrenderer2);
+                plot.setRangeAxis(1, new NumberAxis(this.yLabel2));
+
+                plot.mapDatasetToRangeAxis(0, 0);// 1st dataset to 1st y-axis
+                plot.mapDatasetToRangeAxis(1, 1); // 2nd dataset to 2nd y-axis
+
+
+                createChart();
+                // to see uncomment this and the code in main
+                seeExample();
+
+                break;
+
+        }
+
+    }
+
+    public void createChart() {
+        scatterChart = new JFreeChart(title, new Font("Serif", java.awt.Font.BOLD, 18), plot, true);
+        chartPanel = new ChartPanel(scatterChart);
+        chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        chartPanel.setBackground(Color.white);
+    }
+
+    public void seeExample() {
+
+        add(chartPanel);
+
+        pack();
+        setTitle(title);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public ChartPanel getChart() {
+        return this.chartPanel;
+    }
+
+    public static void main(String[] args) {
+        String c = "USA";
+        String[][] a = { { "SP.DYN.IMRT.IN", "SH.XPD.CHEX.PC.CD", "SH.MED.BEDS.ZS" } };
+        DataAcquisition test = new DataAcquisition(a[0], c, "2010", "2017");
+        // Viewer_Scatter testgrap = new Viewer_Scatter(test.dataStorage, "Years",
+        // "Values", "Chart");
+        // title will later be changed to whatever the analysis is
+        Viewer_Scatter tt = new Viewer_Scatter(test.dataStorage, "Years", "Values", "Title");
+        tt.setVisible(true);
+
+    }
+
+}
---
+++ src/main/java/visualizations/Viewers_Line.java
@@ -0,0 +1,333 @@
+package visualizations;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.block.BlockBorder;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import dataFetch.DataAcquisition;
+import dataFetch.StoredData;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+/**
+ * Line viewer class which creates the line chart
+ * 
+ * @author group21
+ *
+ */
+public class Viewers_Line extends JFrame {
+
+    private StoredData data, data2, data3;
+    private String title;
+    private String series1;
+    private String series2;
+    private String series3;
+    private ArrayList<StoredData> dataStorage;
+    private String xLabel, yLabel;
+    private XYSeriesCollection dataset;
+    private ChartPanel chartPanel;
+    private int vers;
+
+    public Viewers_Line(ArrayList<StoredData> dataStorage, String xLabel, String yLabel, String title) {
+        if (dataStorage.equals(null)) {
+            return;
+        }
+        this.xLabel = xLabel;
+        this.yLabel = yLabel;
+        this.title = title;
+        this.dataStorage = dataStorage;
+
+        draw();
+
+    }
+
+    public Viewers_Line(ArrayList<StoredData> dataStorage, String xLabel, String yLabel, String yLabel2,
+            String title) {
+        if (dataStorage.equals(null)) {
+            return;
+        }
+        this.xLabel = xLabel;
+        this.yLabel = yLabel;
+        this.title = title;
+        this.dataStorage = dataStorage;
+        draw();
+
+    }
+
+    private void draw() {
+        switch (dataStorage.size()) {
+            case 0:
+                break;
+            case 1:
+                this.data = dataStorage.get(0);
+                this.series1 = data.getSeriesName();
+                this.vers = 1;
+                break;
+            case 2:
+                this.data = dataStorage.get(0);
+                this.data2 = dataStorage.get(1);
+                this.series1 = data.getSeriesName();
+                this.series2 = data2.getSeriesName();
+                this.vers = 2;
+                break;
+            case 3:
+                this.data = dataStorage.get(0);
+                this.data2 = dataStorage.get(1);
+                this.data3 = dataStorage.get(2);
+                this.series1 = data.getSeriesName();
+                this.series2 = data2.getSeriesName();
+                this.series3 = data3.getSeriesName();
+                this.vers = 3;
+                break;
+
+        }
+        pop();
+
+    }
+
+    private void pop() {
+        XYSeries seriesLine, seriesLine2, seriesLine3;
+        dataset = null;
+        switch (vers) {
+            case 0:
+                break;
+            case 1:
+                seriesLine = new XYSeries(this.series1);
+                for (int i = 0; i < data.getValues().size(); i++) {
+                    seriesLine.add(data.getYears().get(i), data.getValues().get(i));
+                }
+                dataset = new XYSeriesCollection();
+                dataset.addSeries(seriesLine);
+                createChart();
+                // to see uncomment this and the code in main
+                seeExample();
+
+                break;
+            case 2:
+                seriesLine = new XYSeries(this.series1);
+                seriesLine2 = new XYSeries(this.series2);
+                for (int i = 0; i < data.getValues().size(); i++) {
+                    seriesLine.add(data.getYears().get(i), data.getValues().get(i));
+                }
+                for (int i = 0; i < data2.getValues().size(); i++) {
+                    seriesLine2.add(data2.getYears().get(i), data2.getValues().get(i));
+                }
+                dataset = new XYSeriesCollection();
+                dataset.addSeries(seriesLine);
+                dataset.addSeries(seriesLine2);
+                createChart();
+                // to see uncomment this and the code in main
+                seeExample();
+
+                break;
+            case 3:
+                seriesLine = new XYSeries(this.series1);
+                seriesLine2 = new XYSeries(this.series2);
+                seriesLine3 = new XYSeries(this.series3);
+                for (int i = 0; i < data.getValues().size(); i++) {
+                    seriesLine.add(data.getYears().get(i), data.getValues().get(i));
+                }
+                for (int i = 0; i < data2.getValues().size(); i++) {
+                    seriesLine2.add(data2.getYears().get(i), data2.getValues().get(i));
+                }
+                for (int i = 0; i < data3.getValues().size(); i++) {
+                    seriesLine3.add(data3.getYears().get(i), data3.getValues().get(i));
+                }
+                dataset = new XYSeriesCollection();
+                dataset.addSeries(seriesLine);
+                dataset.addSeries(seriesLine2);
+                dataset.addSeries(seriesLine3);
+
+                createChart();
+                // to see uncomment this and the code in main
+                seeExample();
+
+                break;
+
+        }
+
+    }
+
+    public void createChart() {
+        JFreeChart chart = ChartFactory.createXYLineChart(this.title, this.xLabel, this.yLabel, dataset,
+                PlotOrientation.VERTICAL, true, true, false);
+
+        XYPlot plot = chart.getXYPlot();
+
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesPaint(0, Color.RED);
+        renderer.setSeriesStroke(0, new BasicStroke(2.0f));
+
+        plot.setRenderer(renderer);
+        plot.setBackgroundPaint(Color.white);
+
+        plot.setRangeGridlinesVisible(true);
+        plot.setRangeGridlinePaint(Color.BLACK);
+
+        plot.setDomainGridlinesVisible(true);
+        plot.setDomainGridlinePaint(Color.BLACK);
+
+        chart.getLegend().setFrame(BlockBorder.NONE);
+
+        chart.setTitle(new TextTitle(title, new Font("Serif", java.awt.Font.BOLD, 18)));
+        chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new Dimension(400, 300));
+        chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        chartPanel.setBackground(Color.white);
+        final NumberAxis domainAxis = (NumberAxis) chart.getXYPlot().getDomainAxis();
+        final DecimalFormat format = new DecimalFormat("####");
+        domainAxis.setNumberFormatOverride(format);
+
+    }
+    
+    public ChartPanel getChart() {
+        return this.chartPanel;
+    }
+
+    public void seeExample() {
+        add(chartPanel);
+        pack();
+        setTitle(title);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+
+
+    public static void main(String[] args) {
+//        String c = "CA";
+//        
+//        DataFetcher test = new DataFetcher(a[0], c, "2000", "2021");
+//        Viewers_Line testgrap = new Viewers_Line(test.dataStorage.get(0), "years", "values", "Chart", "Lol");
+//        testgrap.populateLine(1);
+//        testgrap.setVisible(true);
+
+//  String[][] a = { { "EG.USE.PCAP.KG.OE" } };
+        String c = "USA";
+        String[][] ab = { { "SP.DYN.IMRT.IN", "SH.XPD.CHEX.PC.CD", "SH.MED.BEDS.ZS" } };
+        DataAcquisition test = new DataAcquisition(ab[0], c, "2010", "2017");
+        // title will later be changed to whatever the analysis is
+        Viewers_Line tt = new Viewers_Line(test.dataStorage, "Years", "Values", "Title");
+        tt.setVisible(true);
+
+    }
+
+    public StoredData getData() {
+        return data;
+    }
+
+    public void setData(StoredData data) {
+        this.data = data;
+    }
+
+    public StoredData getData2() {
+        return data2;
+    }
+
+    public void setData2(StoredData data2) {
+        this.data2 = data2;
+    }
+
+    public StoredData getData3() {
+        return data3;
+    }
+
+    public void setData3(StoredData data3) {
+        this.data3 = data3;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSeries1() {
+        return series1;
+    }
+
+    public void setSeries1(String series1) {
+        this.series1 = series1;
+    }
+
+    public String getSeries2() {
+        return series2;
+    }
+
+    public void setSeries2(String series2) {
+        this.series2 = series2;
+    }
+
+    public String getSeries3() {
+        return series3;
+    }
+
+    public void setSeries3(String series3) {
+        this.series3 = series3;
+    }
+
+    public ArrayList<StoredData> getDataStorage() {
+        return dataStorage;
+    }
+
+    public void setDataStorage(ArrayList<StoredData> dataStorage) {
+        this.dataStorage = dataStorage;
+    }
+
+    public String getxLabel() {
+        return xLabel;
+    }
+
+    public void setxLabel(String xLabel) {
+        this.xLabel = xLabel;
+    }
+
+    public String getyLabel() {
+        return yLabel;
+    }
+
+    public void setyLabel(String yLabel) {
+        this.yLabel = yLabel;
+    }
+
+    public XYSeriesCollection getDataset() {
+        return dataset;
+    }
+
+    public void setDataset(XYSeriesCollection dataset) {
+        this.dataset = dataset;
+    }
+
+    public ChartPanel getChartPanel() {
+        return chartPanel;
+    }
+
+    public void setChartPanel(ChartPanel chartPanel) {
+        this.chartPanel = chartPanel;
+    }
+
+    public int getVers() {
+        return vers;
+    }
+
+    public void setVers(int vers) {
+        this.vers = vers;
+    }
+}
